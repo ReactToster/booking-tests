b483b06d410d1c664e84cfaab35f9f86e320a55c
Karol Rosiński <krosinski94@gmail.com>
2023-03-22 19:16:03 +0100

    add testSimpleSearch

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,15 +1,19 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class MainTest {
     WebDriver driver;
 
     @BeforeClass
-    void setUpDriver() throws InterruptedException {
+    void setUpDriver() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments(
                 "start-maximized",
@@ -21,7 +25,46 @@ public class MainTest {
     }
 
     @Test
-    void verificationTest() {
+    void testSimpleSearch() {
+        WebElement cookieBtn = driver.findElement(By.cssSelector("#onetrust-accept-btn-handler"));
+        cookieBtn.click();
+        List<WebElement> searchInputList = driver.findElements(By.cssSelector(".hero-banner-searchbox form > div input"));
+
+        WebElement searchLocationInput = searchInputList.get(0);
+        searchLocationInput.sendKeys("Warsaw");
+
+        WebElement searchDateBtn = driver.findElement(By.xpath("//button[@data-testid='date-display-field-start']"));
+        searchDateBtn.click();
+
+        List<WebElement> searchDateTableList = driver.findElements(By.xpath("//table[@role='grid']"));
+        WebElement searchDateTableFromBtn = searchDateTableList.get(0).findElement(By.xpath("//tbody/tr/td/span[@data-date='2023-03-23']"));
+        searchDateTableFromBtn.click();
+
+        WebElement searchDateTableToBtn = searchDateTableList.get(0).findElement(By.xpath("//tbody/tr/td/span[@data-date='2023-03-25']"));
+        searchDateTableToBtn.click();
+
+        searchDateBtn.click();
+
+        WebElement searchOccupancyConfigBtn = driver.findElement(By.xpath("//button[@data-testid='occupancy-config']"));
+        searchOccupancyConfigBtn.click();
+
+        List<WebElement> searchOccupancyBtnList = driver.findElements(By.xpath("//div[@data-testid='occupancy-popup']/div/div/div/button"));
+        WebElement searchOccupancyChildPlusBtn = searchOccupancyBtnList.get(3);
+
+        searchOccupancyChildPlusBtn.click();
+        searchOccupancyChildPlusBtn.click();
+
+        List<WebElement> searchOccupancyChildAgeSelectList = driver.findElements(By.xpath("//select[@name='age']"));
+        List<WebElement> searchOccupancyFirstChildAgeSelectOptions = searchOccupancyChildAgeSelectList.get(0).findElements(By.xpath("//option[@value='5']"));
+        searchOccupancyChildAgeSelectList.get(0).click();
+        searchOccupancyFirstChildAgeSelectOptions.get(0).click();
+
+        List<WebElement> searchOccupancySecondChildAgeSelectOptions = searchOccupancyChildAgeSelectList.get(1).findElements(By.xpath("//option[@value='7']"));
+        searchOccupancyChildAgeSelectList.get(1).click();
+        searchOccupancySecondChildAgeSelectOptions.get(1).click();
+
+        WebElement searchSubmitBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        searchSubmitBtn.click();
     }
 
     @AfterClass
